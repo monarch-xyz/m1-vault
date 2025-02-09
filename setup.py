c8d5913e685f2bf6d291e2b04f2ff2acb85dccb4
antoncoding <antonassocareer@gmail.com>
2025-02-09 21:29:39 +0800

    chore: setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your_bot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp>=3.9.0',
+        'python-dotenv>=0.19.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'start-bot=src.main:main',
+        ],
+    },
+) 
